bf3aa18554f8e700837faaf5a8c9ef3b52ed3a16
Marie <Marie@192.168.0.155>
2020-02-20 09:34:12 +0100

    initial

---
+++ squareroot/src/squareroot/SquareRoot.java
@@ -0,0 +1,13 @@
+package squareroot;
+
+public class SquareRoot {
+	static int squareRoot(int x) {
+		int result = 0;
+		while (result*result <=x) {
+			result ++;
+			
+		}
+		return result-1;
+	}
+}
+	 
---
+++ squareroot/src/squareroot/SquareRootTest.java
@@ -0,0 +1,14 @@
+package squareroot;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SquareRootTest {
+
+	@Test
+	void test() {
+		assert SquareRoot.squareRoot(9)==3;
+	}
+
+}
